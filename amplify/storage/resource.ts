b0cf0a26221e5490c2fe6a1d56265a8ff4928bdc
Nathan McIntyre <nmcintyre@albedo.com>
2024-10-12 13:26:26 -0600

    fix storage path 2

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'amplifyTestStorage',
   access: (allow) => ({
-    '/*': [
+    'data/*': [
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
   })
