1860f30bfbcca14d05d88e2eb6b09bcf4375e6c4
Nathan McIntyre <nmcintyre@albedo.com>
2024-10-12 16:02:32 -0600

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import React from 'react';
-import { uploadData } from 'aws-amplify/storage';
+import { FileUploader } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
-  const [file, setFile] = React.useState();
-
-  const handleChange = (event: any) => {
-    setFile(event.target.files[0]);
-  };
 
   return (
-    <div>
-      <input type="file" onChange={handleChange} />
-        <button
-          onClick={() =>
-            uploadData({
-              path: `data/${file.name}`,
-              data: file,
-          })
-        }
-      >
-        Upload
-      </button>
-    </div>
+	<FileUploader
+      acceptedFileTypes={['*/*']}
+      path="data/"
+      maxFileCount={1}
+      isResumable
+    />
   );
 }
 
