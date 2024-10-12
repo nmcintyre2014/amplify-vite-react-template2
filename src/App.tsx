b0bd274344ffdc3e7aab213ba0bbe17a4575fa4b
Nathan McIntyre <nmcintyre@albedo.com>
2024-10-12 11:20:44 -0600

    storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-function App() {
+import React from 'react';
+import { Storage } from 'aws-amplify' 
 
-  return (
-    <main>
-    <div>
-      Test
-    </div>
-    </main>
-  );
+class App extends React.Component {
+  onChange(e) {
+    const file = e.target.files[0];
+    Storage.put(file.name, file)
+    .then (result => console.log(result))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <input
+        type="file" accept='image/png'
+        onChange={(e) => this.onChange(e)}
+      />
+    )
+  }
 }
 
 export default App;
