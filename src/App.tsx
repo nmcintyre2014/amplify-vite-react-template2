830d569424c7a36ca9a9738812fbfbd674f690a9
Nathan McIntyre <nmcintyre@albedo.com>
2024-10-11 20:08:49 -0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,6 @@ function App() {
   return (
     <div>
       Test
-      <!--
-      <input type="file" onChange={handleChange} />
-        <button
-          onClick={() =>
-            uploadData({
-              path: `picture-submissions/${file.name}`,
-              data: file,
-          })
-        }
-      >
-        Upload
-      </button>
-	-->
     </div>
   );
 }
