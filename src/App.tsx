8675583032354d4f33e4a5db6fdda95563515674
Nathan McIntyre <nmcintyre@albedo.com>
2024-10-12 17:29:08 -0600

    lo button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { FileUploader } from '@aws-amplify/ui-react-storage';
+import { signOut } from 'aws-amplify/auth';
 import '@aws-amplify/ui-react/styles.css';
 
 function App() {
 
+  async function handleSignOut() {
+    await signOut()
+  }
+
   return (
+	<div>
 	<FileUploader
       acceptedFileTypes={['*/*']}
       path="data/"
       maxFileCount={1}
       isResumable
     />
+	<button type="button" onClick={handleSignOut}>Sign out</button>
+	</div>
   );
 }
 
