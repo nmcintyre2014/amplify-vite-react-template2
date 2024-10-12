75ec10969f01cf6c7fc53f3f0a2b39d6eb2bded9
Nathan McIntyre <nmcintyre@albedo.com>
2024-10-11 18:45:33 -0600

    bf

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ const client = generateClient<Schema>();
 
 function App() {
   const { signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   const [file, setFile] = React.useState();
 
@@ -16,40 +15,8 @@ function App() {
     setFile(event.target.files[0]);
   };
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-    
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-		key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
 
       <div>
       <input type="file" onChange={handleChange} />
@@ -63,6 +30,7 @@ function App() {
       >
         Upload
       </button>
+      <button onClick={signOut}>Sign out</button>
     </div>
     </main>
   );
